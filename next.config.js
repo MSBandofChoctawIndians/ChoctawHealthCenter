d7320e84565278ef625038aa08e3211e115dbe30
Sonny Mattera <smattera@hotmail.com>
2023-12-08 19:38:25 -0600

    Update basePath in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/GitHubPagesTemplate',
+  basePath: '/ChoctawHealthCenter',
 }
 
 module.exports = nextConfig
